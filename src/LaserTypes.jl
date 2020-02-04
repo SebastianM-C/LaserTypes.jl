de451d14ef0648d58a2c98cd655735c6ed7c9f55
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-02-04 18:30:45 +0200

    Files generated by PkgTemplates

---
+++ src/LaserTypes.jl
@@ -0,0 +1,5 @@
+module LaserTypes
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LaserTypes
+using Test
+
+@testset "LaserTypes.jl" begin
+    # Write your own tests here.
+end
