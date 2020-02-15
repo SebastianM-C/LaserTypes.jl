52848df0a777206c9dbd9adea6489c453af8f1fb
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-02-15 19:52:43 +0200

    Fix tests

--- test/gauss.jl
+++ test/gauss.jl
@@ -6,14 +6,43 @@ import PhysicalConstants.CODATA2018: c_0, m_e, e, α
 
 @testset "SI units" begin
     p = Gauss.LaserParams()
-    @test dimension(p.c) == dimension(u"m/s")
+    @unpack c, z_F, z_R, k, w₀, E₀ = p
+    wz = LaserTypes.w(z_F, p)
 
-    x₀ = SVector{3}(1,1,0.)u"m"
+    @test unit(c) == u"m/s"
+    @test unit(k) == u"μm^-1"
+    @test unit(z_R) == u"μm"
+
+    x₀ = SVector{3}(0u"μm",0u"μm",z_F)
+    t₀ = 0u"s"
 
     E = Gauss.E
     B = Gauss.B
-    @test all(dimension.(E(x₀,0u"s",p)) .== Ref(dimension(u"V/m")))
-    @test all(dimension.(B(x₀,0u"s",p)) .== Ref(dimension(u"T")))
+
+    @testset "Dimensions" begin
+        @test all(dimension.(E(x₀,t₀,p)) .== Ref(dimension(u"V/m")))
+        @test all(dimension.(B(x₀,t₀,p)) .== Ref(dimension(u"T")))
+    end
+
+    @testset "Units" begin
+        @test all(unit.(E(x₀,t₀,p)) .== unit(p.E₀))
+        @test all(unit.(B(x₀,t₀,p)) .== unit(p.E₀/c))
+    end
+
+    @testset "Values at origin" begin
+        @test Gauss.Ex(0u"μm", 0u"μm", p) ≈ E₀
+        @test iszero(Gauss.Ey(0u"μm", 0u"μm", p))
+        @test iszero(Gauss.Ez(0u"μm", 0u"μm", 0u"μm", 0u"μm", p))
+
+        @test iszero(Gauss.Bx(0u"μm", 0u"μm", p))
+        @test Gauss.By(0u"μm", 0u"μm", p) ≈ E₀ / c
+        @test iszero(Gauss.Bz(0u"μm", 0u"μm", 0u"μm", 0u"μm", p))
+    end
+
+    @testset "Values at z_F" begin
+        @test E(x₀,t₀,p) ≈ [E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R))), 0u"V/m", 0u"V/m"]
+        @test B(x₀,t₀,p) ≈ [0u"T", E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R)))/c, 0u"T"]
+    end
 end
 
 @testset "Atomic units" begin
@@ -23,16 +52,47 @@ end
     λ = auconvert(800u"nm")
     w0 = auconvert(58u"μm")
     τ0 = auconvert(18u"fs")
+
     p = Gauss.LaserParams(c=c, q=q, m_q=m, λ=λ, w₀=w0, τ₀=τ0)
+    @unpack c, z_F, z_R, k, w₀, E₀ = p
+    wz = LaserTypes.w(z_F, p)
+
+    @test unit(c) == u"a0_au*Eh_au/ħ_au"
+    @test unit(k) == u"a0_au^-1"
+    @test unit(z_R) == u"a0_au"
 
     @test p.ω ≈ 0.05695419u"Eh_au/ħ_au"
 
-    x₀ = SVector{3}(1,1,0.)u"a0_au"
+    x₀ = SVector{3}(0u"a0_au",0u"a0_au",z_F)
+    t₀ = 0u"ħ_au/Eh_au"
 
     E = Gauss.E
     B = Gauss.B
-    @test all(dimension.(E(x₀,0u"ħ_au/Eh_au",p)) .== Ref(dimension(u"V/m")))
-    @test all(dimension.(B(x₀,0u"ħ_au/Eh_au",p)) .== Ref(dimension(u"T")))
+
+    @testset "Dimensions" begin
+        @test all(dimension.(E(x₀,t₀,p)) .== Ref(dimension(u"V/m")))
+        @test all(dimension.(B(x₀,t₀,p)) .== Ref(dimension(u"T")))
+    end
+
+    @testset "Units" begin
+        @test all(unit.(E(x₀,t₀,p)) .== unit(p.E₀))
+        @test all(unit.(B(x₀,t₀,p)) .== unit(p.E₀/c))
+    end
+
+    @testset "Values at origin" begin
+        @test Gauss.Ex(0u"a0_au", 0u"a0_au", p) ≈ E₀
+        @test iszero(Gauss.Ey(0u"a0_au", 0u"a0_au", p))
+        @test iszero(Gauss.Ez(0u"a0_au", 0u"a0_au", 0u"a0_au", 0u"a0_au", p))
+
+        @test iszero(Gauss.Bx(0u"a0_au", 0u"a0_au", p))
+        @test Gauss.By(0u"a0_au", 0u"a0_au", p) ≈ E₀ / c
+        @test iszero(Gauss.Bz(0u"a0_au", 0u"a0_au", 0u"a0_au", 0u"a0_au", p))
+    end
+
+    @testset "Values at z_F" begin
+        @test E(x₀,t₀,p) ≈ [E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R))), 0u"V/m", 0u"V/m"]
+        @test B(x₀,t₀,p) ≈ [0u"T", E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R)))/c, 0u"T"]
+    end
 end
 
 @testset "No units" begin
@@ -42,14 +102,31 @@ end
     λ = austrip(800u"nm")
     w0 = austrip(58u"μm")
     τ0 = austrip(18u"fs")
+
     p = Gauss.LaserParams(c=c, q=q, m_q=m, λ=λ, w₀=w0, τ₀=τ0)
+    @unpack c, z_F, z_R, k, w₀, E₀ = p
+    wz = LaserTypes.w(z_F, p)
 
     @test p.ω ≈ 0.05695419
 
-    x₀ = SVector{3}(1,1,0.)
+    x₀ = SVector{3}(0, 0, z_F)
+    t₀ = 0.
 
     E = Gauss.E
     B = Gauss.B
-    @test E(x₀,0,p) ≈ [8.7831165e-7, 0, 0]
-    @test B(x₀,0,p) ≈ [0, 6.409349797e-9, 0]
+
+    @testset "Values at origin" begin
+        @test Gauss.Ex(0, 0, p) ≈ E₀
+        @test iszero(Gauss.Ey(0, 0, p))
+        @test iszero(Gauss.Ez(0, 0, 0, 0, p))
+
+        @test iszero(Gauss.Bx(0, 0, p))
+        @test Gauss.By(0, 0, p) ≈ E₀ / c
+        @test iszero(Gauss.Bz(0, 0, 0, 0, p))
+    end
+
+    @testset "Values at z_F" begin
+        @test E(x₀,t₀,p) ≈ [E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R))), 0, 0]
+        @test B(x₀,t₀,p) ≈ [0, E₀*w₀/wz*real(exp(-im*k*z_F+im*atan(z_F,z_R)))/c, 0]
+    end
 end
