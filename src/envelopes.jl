8e992858b15f6cc59c93bb05235d44be752ed7ef
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-05-15 00:16:08 +0300

    Describe QuasiRectangularProfile

--- src/envelopes.jl
+++ src/envelopes.jl
@@ -18,11 +18,11 @@ struct ConstantProfile end
 This envelope provides a finite duration for the laser pulse and thus can provide a more
 realistic description of an actual laser pulse.
 ```math
-envelope(z, t) = \\cosh\\left[\\left(\\frac{\\varphi}{\\tau})\\right)^2\\right],
+envelope(z, t) = \\cosh\\left[\\left(\\frac{φ}{τ}\\right)^2\\right],
 ```
 where
 ```math
-\\varphi = (t - t_0) - \\frac{z - z_0}{c} ,
+\\varphi = (t - t_0) - \\frac{z - z_0}{c}\\,,
 ```
 and
 - `c` is the speed of light
@@ -42,12 +42,22 @@ end
 @doc """
     QuasiRectangularProfile{V,T,L}
 
-This envelope provides a finite duration for the laser pulse and thus can provide a more
-realistic description of an actual laser pulse.
+This envelope produces a pulse with a predominant constant part of width ``Δz``
+which could offer better results than the Gaussian profile in the paraxial limit
+(which is considered for the spatial profiles). The shape of the envelope is given by
 ```math
 envelope(z, t) =
+    \\begin{cases}
+    \\cosh\\left[\\left(\\frac{φ}{τ}\\right)^2\\right], & \\text{for } φ ≤ 0\\\\
+    1\\,, & \\text{for } φ < Δz\\\\
+    \\cosh\\left[\\left(\\frac{φ - Δz/c}{τ}\\right)^2\\right], & \\text{otherwise}
+    \\end{cases}
 ```
 where
+```math
+\\varphi = (t - t_0) - \\frac{z - z_0}{c}\\,,
+```
+and
 - `c` is the speed of light
 - `τ` is the duration of the pulse (FWHM) and has the default value 18.02fs
 - `t₀` is the origin of the time axis and it is 0 by default
@@ -67,14 +77,16 @@ end
 """
     g(z, t, par)
 
-The time dependence of the fields is given by
+The time dependence of the fields defined by this package is given by
 ```math
-g(z, t) = \\cos(\\omega t) envelope(z, t),
+g(z, t) = \\cos(ω t) envelope(z, t),
 ```
 where
-- ``\\omega`` is the angular frequency of the laser pulse
+- `z` and `t` are the position on the ``Oz`` axis and the time
+- `par` are the laser parameters which pass the corresponding profile to the envelope
+and
+- ``ω`` is the angular frequency of the laser pulse
 - ``envelope(z, t)`` is a function that can be used to control the duration of the pulse
-
 """
 function g(z, t, par)
     @unpack profile, ω = par
