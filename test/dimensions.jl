899573693491bd6a0ad33d56bfbeac2e54609ba9
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-09 03:26:05 +0300

    Fix tests

--- test/dimensions.jl
+++ test/dimensions.jl
@@ -3,7 +3,7 @@ using Unitful
 using StaticArrays
 using UnitfulAtomic
 
-@testset "$laser" for laser in [GaussLaser, LaguerreGaussLaser, Cos²Profile]
+@testset "$laser" for laser in [GaussLaser, LaguerreGaussLaser]
     units = [:SI_unitful, :atomic_unitful]
     x₀ = SVector{3}(0u"μm",0u"μm",0u"μm")
     t₀ = 0u"s"
@@ -14,5 +14,14 @@ using UnitfulAtomic
         s = setup_laser(laser, unit)
         @test all(dimension.(E(xᵢ,tᵢ,s)) .== Ref(dimension(u"V/m")))
         @test all(dimension.(B(xᵢ,tᵢ,s)) .== Ref(dimension(u"T")))
+
+        @testset "Profiles" for profile in (ConstantProfile,
+                                            GaussProfile,
+                                            Cos²Profile,
+                                            QuasiRectangularProfile)
+            s = setup_laser(laser, unit, profile=profile())
+            @test all(dimension.(E(xᵢ,tᵢ,s)) .== Ref(dimension(u"V/m")))
+            @test all(dimension.(B(xᵢ,tᵢ,s)) .== Ref(dimension(u"T")))
+        end
     end
 end
